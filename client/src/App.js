767096ef4168c9ce01e71019f768633ea5249096
tilaksolunke <tilaksolunke2002@gmail.com>
2023-09-22 09:38:32 +0530

    Add new context component

--- client/src/App.js
+++ client/src/App.js
@@ -6,17 +6,20 @@ import Home from "./components/Home/Home";
 import Category from "./components/Category/Category";
 import SingleProduct from "./components/SingleProduct/SingleProduct";
 import Newsletter from "./components/Footer/Newsletter/Newsletter";
-// import AppContext from "./utils/context";
+import AppContext from "./utils/context";
 
 function App() {
     return (
         <BrowserRouter>
+        <Header />
         {/* Its nothing but a pages in our website  */}
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/category/:id" element={<Category />} />
             <Route path="/product/:id" element={<SingleProduct />} />
         </Routes>
+        <Newsletter />
+        <Footer />
         </BrowserRouter>
     );
 }
